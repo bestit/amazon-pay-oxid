ec71dbc5544307c7babcf4a4c00416063030ea2c
R <Ralf.nitzer@bestit-online.de>
2021-02-03 16:21:10 +0100

    OXAP-337 Add a secret key to the cron controller

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec2.0.0.html).
+## [3.6.6] - 2020-02-02
+### Fixed
+- Adds a secret key to the controller
+See the Documenatation how to use the Secret Key in CronJobs [#Secret Key](https://amazon.bestit-support.de/de/oxap/master/einstellungen-statuswechsel#secretkey)
+
 ## [3.6.5] - 2020-02-01
 ### Fixed
 - optimization to the controllers
--- ext/bestitamazonpay4oxid_module_config.php
+++ ext/bestitamazonpay4oxid_module_config.php
@@ -37,7 +37,7 @@ class bestitAmazonPay4Oxid_module_config extends bestitAmazonPay4Oxid_module_con
 
     /**
      * Extends the save config variable function to store the amazon config vars
-     * from the provided config json object.
+     * from the provided config json object or generate a new cron secret key
      * @throws oxSystemComponentException
      */
     public function saveConfVars()
@@ -63,11 +63,54 @@ class bestitAmazonPay4Oxid_module_config extends bestitAmazonPay4Oxid_module_con
                         $_POST[$sMainKey][$sSubKey] = $aQuickConfig[$sAmazonKey];
                     }
                 }
+
+                if ((bool) $_POST['confbools']['blGenerateNewAmazonCronSecretKey'] === true) {
+                    $_POST['confbools']['blGenerateNewAmazonCronSecretKey'] = false;
+                    $_POST['confstrs']['sAmazonCronSecretKey'] = self::_generatePassword();
+                }
             } catch (\Exception $oException) {
                 //Do nothing
             }
         }
 
         $this->_parentSaveConfVars();
+    }
+
+    /**
+     * @see https://gist.github.com/tylerhall/521810
+     * Generates a strong password of N length containing at least one lower case letter,
+     * one uppercase letter and one digit. The remaining characters
+     * in the password are chosen at random from those four sets.
+     *
+     * The available characters in each set are user friendly - there are no ambiguous
+     * characters such as i, l, 1, o, 0, etc. This makes it much easier for users to manually
+     * type or speak their passwords.
+     *
+     * @param int $length
+     *
+     * @return string
+     */
+    protected static function _generatePassword($length = 15)
+    {
+        $sets = array();
+        $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+        $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+        $sets[] = '23456789';
+
+        $pool = '';
+        $password = '';
+
+        foreach ($sets as $set) {
+            $password .= $set[array_rand(str_split($set))];
+            $pool .= $set;
+        }
+
+        $pool = str_split($pool);
+        for ($i = 0; $i < $length - count($sets); ++$i) {
+            $password .= $pool[array_rand($pool)];
+        }
+        $password = str_shuffle($password);
+
+        return $password;
     }
 }
